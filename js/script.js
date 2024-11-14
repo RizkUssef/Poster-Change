96bb976253957fac89fc7f14c1c20559b555d642
Rizk Ussef <rizkusgt67@gmail.com>
2024-11-14 17:36:47 +0200

    add active class to choosen image

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 let poster = document.getElementById("poster");
+let imgs = Array.from(document.querySelectorAll(".inner-imgs img"));
 let img_container = document.getElementById("all-imgs");
-
 img_container.addEventListener("click", function(e) {
     if(e.target.tagName == "IMG"){
         let new_src= e.target.getAttribute("src");
+        document.querySelector(".active").classList.remove("active");
+        e.target.classList.toggle("active");
         poster.setAttribute("src", new_src);
+
     }
 })
